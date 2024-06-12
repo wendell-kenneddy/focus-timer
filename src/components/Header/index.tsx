cefcef1c50fdd63733281bd99d80db84495d66b0
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-12 18:37:06 -0300

    feat(components): add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { SwitchThemeButton } from "../SwitchThemeButton";
+
+export function Header() {
+  return (
+    <header className="w-full p-4 flex items-center justify-center">
+      <SwitchThemeButton />
+    </header>
+  );
+}
