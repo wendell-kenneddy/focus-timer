6345223379af0253f56c0444582a4548b465e5fe
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-12 18:37:28 -0300

    feat(components): update App structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,19 @@
+import { useTheme } from "../hooks/useTheme";
+import { Header } from "./Header";
+import { Timer } from "./Timer";
+
 export function App() {
+  const { theme } = useTheme();
+  const bgColor = theme == "light" ? "bg-[#f8f8fc]" : "bg-[#121214]";
+  const textColor = theme == "dark" ? "text-[#f8f8fc]" : "text-[#121214]";
+
   return (
-    <>
-      <h1>Focus Timer</h1>
-    </>
+    <div className={`${bgColor} ${textColor} w-screen h-screen`}>
+      <Header />
+
+      <main>
+        <Timer />
+      </main>
+    </div>
   );
 }
