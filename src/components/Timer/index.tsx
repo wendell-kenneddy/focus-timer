54e3d800db06dfd5c3545e1ca4884ff680550365
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-14 09:39:20 -0300

    feat(Timer): change initial timer duration to 25 minutes

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,9 +3,7 @@ import { TimerControls } from "./TimerControls";
 export function Timer() {
   return (
     <section className="my-[178px] flex flex-col items-center justify-center gap-4 w-[90%] max-w-[500px] mx-auto">
-      <h2 className="sr-only">Timer controls</h2>
-
-      <TimerControls initialTime={1000 * 60 * 2} />
+      <TimerControls initialTime={1000 * 60 * 25} />
     </section>
   );
 }
