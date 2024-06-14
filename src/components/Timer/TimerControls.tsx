f8fbdc002801d1dd7c27886107714948af6135c3
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-14 09:38:24 -0300

    feat(TimerControls): use timerReducer and TimerDialog

--- src/components/Timer/TimerControls.tsx
+++ src/components/Timer/TimerControls.tsx
@@ -6,9 +6,12 @@ import {
   SpeakerNone,
   SpeakerHigh,
 } from "@phosphor-icons/react";
-import { useRef, useState } from "react";
+import { useReducer, useRef, useState } from "react";
 import { useTheme } from "../../hooks/useTheme";
 import { millisToMinutesAndSeconds } from "../../utils/millisToMinutesAndSeconds";
+import { TimerDialog } from "./TimerDialog";
+import { spawnNotification } from "../../utils/spawnNotification";
+import { timerReducer } from "../../reducers/timerReducer";
 
 interface TimerControlsProps {
   initialTime: number;
@@ -24,63 +27,76 @@ export type ClockStatus = "paused" | "running" | "inactive";
 export type AlarmStatus = "muted" | "unmuted";
 
 export function TimerControls({ initialTime }: TimerControlsProps) {
-  const [clockStatus, setClockStatus] = useState<"paused" | "running" | "ended">("ended");
-  const [alarmStatus, setAlarmStatus] = useState<"muted" | "unmuted">("unmuted");
-  const [totalTime, setTotalTime] = useState(initialTime);
+  const [timerData, dispatchTimer] = useReducer(timerReducer, {
+    clockStatus: "inactive",
+    alarmStatus: "unmuted",
+    currentDuration: initialTime,
+    totalDuration: initialTime,
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const alarmRef = useRef(document.getElementById("alarm") as HTMLAudioElement);
+  const intervalId = useRef(0);
+  const formattedTime = millisToMinutesAndSeconds(timerData.currentDuration);
   const { theme } = useTheme();
-  const textColor = theme == "dark" ? "text-[#f8f8fc]" : "text-[#121214]";
-  let intervalId = useRef(0);
+  const textColor = theme == "dark" ? "text-grayish-white" : "text-light-black";
 
-  if (totalTime == -1000) {
-    stopClock();
-    setTotalTime(initialTime);
-    alert("Timer ended!");
+  if (timerData.clockStatus == "running") {
+    document.title = `Focus Timer | ${formattedTime}`;
   }
 
-  function openSettingsModal() {}
+  if (timerData.currentDuration < 0) {
+    playAlarm();
+    dispatchTimer({ type: "inactivate-clock", args: intervalId.current });
+    spawnNotification("Timer ended!", {
+      body: "Shall we go for another round?",
+    });
+  }
+
+  function changeTotalDuration(newDuration: number) {
+    clearInterval(intervalId.current);
+    dispatchTimer({ type: "set-total-duration", args: newDuration });
+  }
+
+  function playAlarm() {
+    if (alarmRef.current.HAVE_ENOUGH_DATA && timerData.alarmStatus == "unmuted") {
+      alarmRef.current.play();
+    }
+  }
 
   function runClock() {
-    if (clockStatus != "running") setClockStatus("running");
-    intervalId.current = setInterval(() => {
-      setTotalTime((prevTime) => prevTime - 1000);
-    }, 1000);
+    if (timerData.clockStatus != "running") dispatchTimer({ type: "activate-clock" });
+    intervalId.current = setInterval(() => dispatchTimer({ type: "tick-clock" }), 1000);
   }
 
-  function pauseClock() {
-    clearInterval(intervalId.current);
-    setClockStatus("paused");
-  }
-
-  function stopClock() {
-    clearInterval(intervalId.current);
-    setClockStatus("ended");
-    setTotalTime(initialTime);
-  }
-
-  function handleClockButtonClick() {
-    if (clockStatus == "running") return pauseClock();
+  async function handleClockButtonClick() {
+    if (window.Notification && Notification.permission != "denied") {
+      await Notification.requestPermission();
+    }
+    if (timerData.clockStatus == "running")
+      return dispatchTimer({ type: "pause-clock", args: intervalId.current });
     runClock();
   }
 
   function handleMiddleButtonClick() {
-    if (clockStatus == "running" || clockStatus == "paused") return stopClock();
-    openSettingsModal();
+    if (timerData.clockStatus != "inactive")
+      return dispatchTimer({ type: "inactivate-clock", args: intervalId.current });
+    setIsModalOpen(true);
   }
 
   function handleAlarmButtonClick() {
-    if (alarmStatus == "muted") return setAlarmStatus("unmuted");
-    setAlarmStatus("muted");
+    if (timerData.alarmStatus == "muted") return dispatchTimer({ type: "unmute-alarm" });
+    dispatchTimer({ type: "mute-alarm" });
   }
 
   return (
     <>
-      <span className={`${textColor} font-bold text-8xl`}>
-        {millisToMinutesAndSeconds(totalTime)}
-      </span>
+      <h2 className="sr-only">Timer controls</h2>
 
-      <div className="flex items-center justify-center gap-4 rounded-full bg-[#242424] py-[22px] px-10">
+      <span className={`${textColor} font-bold text-8xl`}>{formattedTime}</span>
+
+      <div className="flex items-center justify-center gap-4 rounded-full bg-surface-black py-[22px] px-10">
         <button name="Pause or resume timer" onClick={handleClockButtonClick}>
-          {clockStatus == "ended" || clockStatus == "paused" ? (
+          {timerData.clockStatus == "inactive" || timerData.clockStatus == "paused" ? (
             <PlayCircle size={64} color="#42d3ff" weight="regular" />
           ) : (
             <PauseCircle size={64} color="#42d3ff" weight="regular" />
@@ -91,7 +107,7 @@ export function TimerControls({ initialTime }: TimerControlsProps) {
           name="Stop ongoing timer or open options when no timer is running"
           onClick={handleMiddleButtonClick}
         >
-          {clockStatus != "ended" ? (
+          {timerData.clockStatus != "inactive" ? (
             <StopCircle size={64} color="#42d3ff" weight="regular" />
           ) : (
             <Timer size={64} color="#42d3ff" weight="regular" />
@@ -99,12 +115,20 @@ export function TimerControls({ initialTime }: TimerControlsProps) {
         </button>
 
         <button name="Mute or unmute alarm" onClick={handleAlarmButtonClick}>
-          {alarmStatus == "muted" ? (
-            <SpeakerNone size={64} color="#42d3ff" weight="regular" />
-          ) : (
+          {timerData.alarmStatus == "muted" ? (
             <SpeakerHigh size={64} color="#42d3ff" weight="regular" />
+          ) : (
+            <SpeakerNone size={64} color="#42d3ff" weight="regular" />
           )}
         </button>
+
+        <TimerDialog
+          isModalOpen={isModalOpen}
+          onSubmit={changeTotalDuration}
+          onOpenChange={setIsModalOpen}
+        >
+          <button className="sr-only">Open settings</button>
+        </TimerDialog>
       </div>
     </>
   );
